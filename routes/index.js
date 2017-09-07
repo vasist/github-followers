9d51b5a995a2e07d872f7b7c4b1d05becc5469d7
vasist <dinnumlg@gmail.com>
2017-09-07 12:38:46 -0700

    get followers

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-const request = require('request-promise')
+const request = require('request-promise');
+const _ = require('lodash');
+const async = require('async');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -9,13 +11,41 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/followers/:user', function (req, res, next) {
-  var user = req.params.user;
-  if (!user) {
-    res.send({ error: 'Enter a valid github id.' })
-  }
+  let user = req.params.user;
+  async.auto({
+    followers: function (callback) {
+      getFollowers(user).then(function (result) {
+        callback(null, result);
+      }, function (err) {
+        callback(err);
+      })
+    },
+    getFollowersSecondLevel: ['followers', function (result, callback) {
+      var actions = result.followers.map(getFollowers); 
+      Promise.all(actions)
+        .then(function (result) {
+          res.send(result);
+          callback(null, result);
+        }, function (err) {
+          callback(err);
+        })
+    }],
+    getFollowersThirdLevel: ['getFollowersSecondLevel', function (result, callback) {
+      callback(null, result)
+    }]
+  }, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(JSON.parse(result));
+    }
+  })
+})
+
+function getFollowers(username) {
   const options = {
     method: 'GET',
-    uri: 'https://api.github.com/users/' + user + '/followers',
+    uri: 'https://api.github.com/users/' + username + '/followers',
     headers: {
       'User-Agent': 'request'
     },
@@ -23,15 +53,16 @@ router.get('/followers/:user', function (req, res, next) {
       per_page: 5
     }
   }
-  request(options)
-    .then(function (response) {
-      debugger;
-      res.send(response);
-    })
-    .catch(function (response) {
-      debugger;
-      res.send(response);
-    })
-})
+  return new Promise((resolve, reject) => {
+    request(options)
+      .then(function (response) {
+        let followers = _.map(JSON.parse(response), 'login');
+        resolve(followers);
+      })
+      .catch(function (response) {
+        reject(response);
+      })
+  })
+}
 
 module.exports = router;
